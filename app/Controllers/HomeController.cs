15be017d32a847a381d4536cfa72973a9d6149b4
Tom Deseyn <tom.deseyn@gmail.com>
2017-06-29 11:35:10 +0200

    Update for 2.0

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using app.Models;
 
-namespace WebApplication.Controllers
+namespace app.Controllers
 {
     public class HomeController : Controller
     {
@@ -29,7 +31,7 @@ namespace WebApplication.Controllers
 
         public IActionResult Error()
         {
-            return View();
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
