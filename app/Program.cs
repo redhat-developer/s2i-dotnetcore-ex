ba8ac9402bd6a3ad247ba6c109bc23f3601e3fce
Tom Deseyn <tom.deseyn@gmail.com>
2017-07-11 16:51:49 +0200

    Update for 2.0 (#29)

--- app/Program.cs
+++ app/Program.cs
@@ -1,32 +1,25 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
-namespace WebApplication
+namespace app
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddEnvironmentVariables("")
-            .Build();
-
-            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
-
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseUrls(url)
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
