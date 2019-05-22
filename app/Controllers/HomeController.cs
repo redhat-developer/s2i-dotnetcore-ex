6add620e9032514d57bf5778d5fcf2bcfc7b3af1
PipelineSpace <42654681+pipelinespace@users.noreply.github.com>
2019-05-21 19:20:30 -0500

    Update HomeController.cs

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace app.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application, description page.";
 
             return View();
         }
