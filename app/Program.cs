c406a209da667090dae252be22017526fcd9f323
Suresh Kalavala <suresh.kalavala@ibm.com>
2021-04-14 19:47:07 -0400

    fixed warning

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
 
 namespace app
 {
