d9a52ad32a5eec826af87c83b6bbd62d737c0bfb
Shalini Kannaujia <68801205+shal05@users.noreply.github.com>
2022-09-07 15:37:41 +0530

    Update HomeController.cs

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace app.Controllers;
 
 public class HomeController : Controller
 {
+
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
