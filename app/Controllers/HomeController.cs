f9fbb84660dec2e04339d52b77cff9e5d467c8fc
Tom Deseyn <tom.deseyn@gmail.com>
2025-11-17 12:36:47 +0100

    10.0: replace app with 'dotnet new mvc --no-restore' (#73)

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace app.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
